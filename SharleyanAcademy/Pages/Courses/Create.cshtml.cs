5e0f5157dc459a940cfa93ac2d4dc5551fb2e115
Isaac Khounborine <KhounborineII@hendrix.edu>
2021-10-25 18:56:30 -0500

    Part 6 Complete

---
+++ SharleyanAcademy/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SharleyanAcademy.Data;
+using SharleyanAcademy.Models;
+
+namespace SharleyanAcademy.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SharleyanAcademy.Data.SchoolContext _context;
+
+        public CreateModel(SharleyanAcademy.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
