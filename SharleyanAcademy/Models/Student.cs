0540b438bebc181eed8ec418bb2f20fb2653f944
Isaac Khounborine <KhounborineII@hendrix.edu>
2021-10-25 17:32:21 -0500

    Part 2 Complete

--- SharleyanAcademy/Models/Student.cs
+++ SharleyanAcademy/Models/Student.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SharleyanAcademy.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "First/Middle Name")]
         public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
         public int Age { get; set; }
 
